a027910791643f9aec4e763a1e318684f76659eb
lucas-jb <lucasjuarezbarrios@gmail.com>
2023-01-07 21:19:02 +0100

    add bbdd

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
+<?php
+    
+    $conn = mysqli_connect('localhost', 'lucas', 'lucas', 'lucas-jb-pizzas');
+
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+    $result  = mysqli_query($conn, $sql);
+
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+    mysqli_free_result($result);
+    mysqli_close($conn);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <?php include('templates/header.php'); ?>
+
+    <h4 class="center grey-text">Pizzas!</h4>
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza){ ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php print(htmlspecialchars($pizza['title'])); ?></h6>
+                            <ul>
+                                <?php
+                                    $ings = (explode(',', $pizza['ingredients']));
+                                    foreach ($ings as $ing){
+                                        print('<li>' . $ing . '</li>' );
+                                    }
+                                ?>
+                            </ul>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">More info</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
     <?php include('templates/footer.php'); ?>
 </html>
