29166c0f82124f978d0f0b4fddad7e431aeb7d12
lucas-jb <lucasjuarezbarrios@gmail.com>
2023-01-07 18:21:55 +0100

    add footer

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php include('templates/header.php'); ?>
-<body>
-    
-</body>
+    <?php include('templates/footer.php'); ?>
 </html>
---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+    <footer class="section">
+        <div class="center grey-text">Copyright 2023 lucas-jb pizza</div>
+    </footer>
+</body>
