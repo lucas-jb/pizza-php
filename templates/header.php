55fdfa361d6f1c020862089627209bbd5d68928a
lucas-jb <lucasjuarezbarrios@gmail.com>
2023-01-07 18:10:28 +0100

    first commit

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<head>
+    <title>Pizza-php</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+    <body class="grey lighten-4">
+        <nav class="white z-depth-0">
+            <div class="container">
+                <a href="#" class="brand-logo brand-text">lucas-jb pizza</a>
+                <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+                </ul>
+            </div>
+        </nav>
