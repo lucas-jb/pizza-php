cc5307e903dcb502b65aaf97d3cf61ef54051eab
lucas-jb <lucasjuarezbarrios@gmail.com>
2023-01-08 13:36:22 +0100

    add delete

--- details.php
+++ details.php
@@ -2,6 +2,19 @@
 
 include('config/db_connect.php');
 
+
+if(isset($_POST['delete'])){
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+    
+    if(mysqli_query($conn, $sql)){
+        header('Location: index.php');
+    }else{
+        print('query error' . mysqli_error($conn));
+    }
+}
+
 if (isset($_GET['id'])) {
 
     $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -26,6 +39,11 @@ if (isset($_GET['id'])) {
             <p><?php print(date($pizza['created_at'])); ?></p>
             <h5>Ingredients:</h5>
             <p><?php print(htmlspecialchars($pizza['ingredients'])); ?></p>
+
+            <form action="details.php" method="POST">
+                <input type="hidden" name="id_to_delete" value="<?php print($pizza['id']) ?>"/>
+                <input type="submit" name="delete" value="<?php print('Delete ' . $pizza['title']); ?>" class="btn brand z-depth-0"/>
+            </form>
         <?php else : ?>
             <h5>No such pizza exists!</h5>
         <?php endif; ?>
